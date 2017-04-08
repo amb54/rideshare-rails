8cde60da737621e329ef3a2afc9e9f30e60c4401
amb54 <ambarklund@hotmail.com>
2017-04-08 14:03:02 -0700

    Added lib/seeds copied the csv files in support into thecreated folder. Updated the file db/seeds.rb with code to read in data from csv files into the model.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,38 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+require 'csv'
+
+#PASSENGER
+csv_text = File.read(Rails.root.join('lib', 'seeds', 'passengers.csv'))
+csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
+csv.each do |row|
+  r = Passenger.new
+  r.name = row['name']
+  r.phone_num = row['phone_num']
+  r.save
+end
+
+#DRIVER
+  csv_text = File.read(Rails.root.join('lib', 'seeds', 'drivers.csv'))
+  csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
+  csv.each do |row|
+    d = Driver.new
+    d.name = row['name']
+    d.vin = row['vin']
+    d.save
+end
+
+#TRIPS
+csv_text = File.read(Rails.root.join('lib', 'seeds', 'trips.csv'))
+csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
+csv.each do |row|
+  t = Trip.new
+  t.driver_id= row['driver_id'].to_i
+  t.passenger_id = row['rider_id'].to_i
+  t.date = row['date']
+  t.rating = row['rating'].to_i
+  t.save
+end
